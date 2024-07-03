358bb11a104809b55e256faa9e0a7976f68db8f7
Aswin Asokan <86108610+aswin-asokan@users.noreply.github.com>
2024-07-03 23:00:54 +0530

    push task 5
    task 5 uploaded

---
+++ task5/README.md
@@ -0,0 +1,3 @@
+# Flutter project
+
+This folder contains the files of the flutter project build during this workshop.
---
+++ task5/lib/login.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool? isChecked = false;
+  TextEditingController email = new TextEditingController();
+  TextEditingController pass = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.all(25),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome back",
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+              ),
+              Text(
+                "Login to your account",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextField(
+                controller: email,
+                decoration: InputDecoration(
+                    hintText: "Email",
+                    border: OutlineInputBorder(),
+                    hintStyle: TextStyle(color: Colors.black)),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: pass,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Password",
+                    border: OutlineInputBorder(),
+                    hintStyle: TextStyle(color: Colors.black)),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                      value: isChecked,
+                      onChanged: (newBool) {
+                        setState(() {
+                          isChecked = newBool;
+                        });
+                      }),
+                  Text(
+                    "Remember me",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  Spacer(),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Forgot password?",
+                        style:
+                            TextStyle(fontSize: 15, color: Colors.blueAccent),
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5))),
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      width: double.infinity,
+                      child: Text("Login now",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 17,
+                              color: Colors.white)),
+                    ),
+                  )),
+              SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black87,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5))),
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      width: double.infinity,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                              height: 25,
+                              // decoration: BoxDecoration(color: Colors.blue),
+                              child: Image.asset("assets/images/google.png")),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text("Continue with Google",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 17,
+                                  color: Colors.white)),
+                        ],
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
